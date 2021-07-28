bbd6240912f7934c2c0f4be636e711791e8efaaa
clctamlsc <86610446+clctamlsc@users.noreply.github.com>
2021-07-28 22:01:15 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+window.onload = main;
+
+function main() {
+	
+	let bttns = document.querySelectorAll('.bttn');
+
+	for (let bttn of bttns) {
+		bttn.addEventListener('click', toggle);
+	}
+
+    function toggle(event) {
+    	let status = event.target.innerHTML;
+        let id = event.target.id;
+        let block;
+        if (id == 'blk1') {
+    		block = document.querySelector('#gp1');
+    	} else if (id == 'blk2') {
+    		block = document.querySelector('#gp2');
+    	} else if (id == 'blk3') {
+			block = document.querySelector('#gp3');
+    	} else {
+    		block = document.querySelector('#gp4');
+    	}
+    	if (status == 'Show') {
+    		block.classList.toggle('Off');
+    		event.target.innerHTML = 'Hide';
+    	} else {
+    		block.classList.toggle('Off');
+    		event.target.innerHTML = 'Show';
+    	}
+    }
+
+}
